b0553dbeab65efad5c06b8ded4e870bb8c988f0f
RD <gusdewa@gmail.com>
2016-03-18 16:14:37 +0700

    Remove Screenshot

--- wp-content/themes/raloka-child/functions.php
+++ wp-content/themes/raloka-child/functions.php
@@ -1,8 +1,5 @@
 <?php
-//
-// Recommended way to include parent theme styles.
-//  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
-//  
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -20,6 +17,3 @@ function theme_enqueue_styles() {
     }
     
 }
-//
-// Your code goes below
-//
